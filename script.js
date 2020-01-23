33bc719839df1eba7e5cdba789e09fd7d6ccf186
MariyonJohnson <60225074+MariyonJohnson@users.noreply.github.com>
2020-01-23 09:06:59 -0600

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,16 @@
+// Guided Practice
+$(".replyYes").click(function() {  
+    $(".homers-reply").text("Mariyon's done");
+    $(".replyYes").css("background-color", "yellow");
+    $("body").css("background-color" , "yellow");
+    
+});
+
+//Independent Practice
+$(".replyNo").click(function() {  
+$(".homers-repl").text("Doh");
+    $(".replyNo").css("background-color", "red");      
+$("body").css("background-color" , "red");    
+});
+
+
